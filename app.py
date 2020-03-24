2b9f202eedf3825d6fdf451cacaf7ba33f03cb91
glade46 <14358258+manasir66@users.noreply.github.com>
2020-03-25 00:56:27 +0800

    Add existing file 3

--- app.py
+++ app.py
@@ -4,6 +4,37 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+
+ENV = 'dev'
+
+if ENV == 'dev':
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost/survey'
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Feedback(db.Model):
+    __tablename__ = 'feedback'
+    id = db.Column(db.Integer, primary_key= True)
+    student = db.Column(db.String(200), unique=True)
+    matric_number = db.Column(db.Integer, unique=True)
+    mahallah = db.Column(db.String(200))
+    rating = db.Column(db.Integer)
+    comments = db.Column(db.Text())
+
+    def __init__(self, student, matric_number, mahallah, rating, comments):
+        self.student = student
+        self.matric_number = matric_number
+        self.mahallah = mahallah
+        self.rating = rating
+        self.comments = comments
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
